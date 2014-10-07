1a5843840ce1bc09485071259e0648aaf8f52a9a
Danielle <danielleheckman5@gmail.com>
2014-10-06 20:00:32 -0400

    second commit

--- app/models/life_event.rb
+++ app/models/life_event.rb
@@ -1,5 +1,7 @@
 class LifeEvent < ActiveRecord::Base
 validates :title, presence: true
+validates :title, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
 validates :year, presence: true
 validates :description, length: { maximum: 140 }, allow_blank: true
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -2,5 +2,7 @@ class School < ActiveRecord::Base
 validates :name, presence: true
 validates :beginning_year, presence: true
 validates :ending_year, presence: true
+validates :name, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
 validates_numericality_of :ending_year , :greater_than_or_equal_to => :beginning_year
 end
