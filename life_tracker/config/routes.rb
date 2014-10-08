1a9a5c9c7d21464497d76d18b42d13dde0724668
Will <wrussell35@gmail.com>
2014-10-08 08:21:57 -0400

    test - git giving me problems

--- life_tracker/config/routes.rb
+++ life_tracker/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  resources :people
+
   resources :life_events
 
   resources :schools
@@ -7,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
